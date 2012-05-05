a498d5d2476761548bfc05cccf0bef804ad396ac
Pedro Veloso <pedro.n.veloso@gmail.com>
2012-05-05 20:28:10 +0100

    canvas working, but does nothing usefull right now

--- dart/MainGameScreen.dart
+++ dart/MainGameScreen.dart
@@ -21,9 +21,27 @@ class MainGameScreen {
   // draws a single frame of the game
   void drawFrame() {
     document.query('#status').innerHTML = "chegou aqui 3";
-    ctx.clearRect(0, 0, 1024, 300);
+    ctx.beginPath();
+    ctx.clearRect(0, 0, 1024, 400);
+    ctx.setLineWidth(2);
+    ctx.setFillColor(BACKGROUND_COLOR);
+    ctx.setStrokeColor(BACKGROUND_COLOR);
+    ctx.fill();
+    
+    ctx.closePath();
+    
+    //draw line just to know it's working for now
+    ctx.moveTo(0, 0);
+    ctx.lineTo(500, 400);
+    ctx.stroke();
+  }
+  
+  // draws a banana at X,Y and with a given text
+  void drawBanana(num x, num y, String text){
+    //ctx.drawImage(canvas_OR_image_OR_video, sx_OR_x, sy_OR_y);  
   }
   
   //variables here
   CanvasRenderingContext2D ctx;
+  static final String BACKGROUND_COLOR = "orange";
 }
