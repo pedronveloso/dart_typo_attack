f08a667e31fb56a9153d48a657e0f95c6813bcac
Joao Oliveira <joaoxsouls@gmail.com>
2012-05-05 20:57:09 +0100

    cenas

--- typo_attack.dart
+++ typo_attack.dart
@@ -1,20 +1,23 @@
 #import('dart:html');
 
-class typo_attack {
-
-  typo_attack() {
+class profilemanager{
+  int avatar;
+  string name;  
+  private loadavatarselection(){
+    var avatarslist = document.query('#avatarslist');
+    avatarslist.on.click.add((event) {
+     if (event.target.id.contains('[0-9]')){
+        if (this.avatar != null) this.avatar.classname.remove('avatarselected');
+        document.query('#avatar'+event.target.id.substring(event.target.id.length)).className.add('avatarselected');
+        this.avatar = event.target;
+     }
+    });
   }
-
-  void run() {
-    write("Hello World!");
-  }
-
-  void write(String message) {
-    // the HTML library defines a global "document" variable
-    document.query('#status').innerHTML = message;
+  public run(){
+    loadavatarselection();
   }
 }
 
 void main() {
-  new typo_attack().run();
+  new profilemanager().run();
 }
